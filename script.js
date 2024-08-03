3a9d9a2c5c31a301c2c190d991ffd5ad5a11e83a
fashton28 <fashton502@gmail.com>
2024-08-03 12:30:12 -0400

    finish login functionalities

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-// login components
+document.addEventListener('DOMContentLoaded', (event) => {
+    const loginButton = document.getElementById("button");
+    const testButton = document.getElementById("test");
+    const authForm = document.getElementById("authForm");
+    let users = JSON.parse(localStorage.getItem('users')) || [];
 
-const emailLogged = document.getElementById("email").value;
-const passwordLogged = document.getElementById("password").value;
-const login = document.getElementById("button");
-const testButton = document.getElementById("test");
+    // Sign up function
+    const signUp = () => {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const user = users.find(user => user.email === email && user.password === password);
+        if (user) {
+            alert("User is Already a Member!");
 
+        } else {
+            users.push({ email: email, password: password });
+            localStorage.setItem('users', JSON.stringify(users));
 
-
-testButton.addEventListener("click",function(){
-
-})
-
-
-
-login.addEventListener("click",function(){
-
-    if(localStorage.getItem("name") === emailLogged && localStorage.getItem("pass") === passwordLogged)  {
-        window.location.href = "home.html"
-    }else {
-        alert("login Couldn't be succesful");
+            alert("Sign up successful");
+        }
+        
     }
-})
+
+    // Login function
+    const loginFunc = (e) => {
+        e.preventDefault(); // Prevent form from submitting and reloading the page
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const user = users.find(user => user.email === email && user.password === password);
+        if (user) {
+            alert("Login successful");
+        } else {
+            alert("Wrong email or password");
+        }
+    }
+
+    testButton.addEventListener("click", signUp);
+    authForm.addEventListener("submit", loginFunc);
+});
